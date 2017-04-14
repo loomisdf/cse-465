1b40e24bd56050bb29f485e8032cb81ebd395ae3
loomisdf <loomisdf@gmail.com>
2017-04-14 14:37:03 -0400

    finish part 1 of HW5

--- HW5/myset.cpp
+++ HW5/myset.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 #include "myset.h"
 
+void printValues(myset* set) {
+	int size;
+	int* values = set->getValues(size);
+
+	std::cout << "Values: [ ";
+	for(int i = 0; i < set->size(); i++) {
+		std::cout << values[i] << " ";
+	}
+	std::cout << "]" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
+	myset set;
+	for(int i = 0; i < 10; i++) {
+		set.insert(i);
+	}
+	set.insert(10);
+
+	std::cout << "Size: " << set.size() << std::endl;
+	std::cout << "Capacity: " << set.capacity() << std::endl;
+	printValues(&set);
+
+	std::cout << "insert(5): " << set.insert(5) << std::endl;
+	std::cout << "insert(20): " << set.insert(20) << std::endl;
+	printValues(&set);
+	std::cout << "Size: " << set.size() << std::endl;
+	std::cout << "remove(5): " << set.remove(5) << std::endl;
+	printValues(&set);
+	std::cout << "Size: " << set.size() << std::endl;
+
+	std::cout << "\ncopied set:" << std::endl;
+	myset set_copy(set);
+	printValues(&set_copy);
+
+	std::cout << "&set: " << &set << std::endl;
+	std::cout << "&set_copy: " << &set_copy << std::endl;
+	return 0;
 }
--- HW5/myset.h
+++ HW5/myset.h
@@ -13,55 +13,144 @@ class myset {
 public:
 	static const int MIN_CAPACITY = 10;
 
-	int *data;	// must be dynamically allocate 1D array
+	int *data;		// must be dynamically allocate 1D array
+	int _size; 		// # of items in the set
+	int _capacity; 	// storage capacity of the set
+
 	myset() {
-		data = new int [MIN_CAPACITY];	
+		data = new int [MIN_CAPACITY];
+		_capacity = MIN_CAPACITY;
+		_size = 0;
 	}
+
 	~myset() {
+		delete []data;
 	}
+
 	myset(const myset &other) {
+		_capacity = other.capacity();
+		_size = other.size();
+		data = new int[_capacity];
+
+		memcpy(data, other.data, _capacity * sizeof(int));
 	}
+
 	myset &operator = (const myset &other) {
+		if(this != &other) {
+			delete []data;
+			_capacity = other.capacity();
+			_size = other.size();
+			data = new int[_capacity];
+			memcpy(data, other.data, _capacity * sizeof(int));
+		}
 		return *this;
 	}
+
 	myset(myset &&other) {
+		data = other.data;
+		_size = other._size;
+		_capacity = other._capacity;
+
+		other.data = nullptr;
+		other._size = 0;
+		other._capacity = MIN_CAPACITY;
 	}
+
 	myset &operator = (myset &&other) 	{
+		if(this != &other) {
+			delete []data;
+			data = other.data;
+			_capacity = other._capacity;
+			_size = other._size;
+
+			other.data = nullptr;
+			other._size = 0;
+			other._capacity = MIN_CAPACITY;
+		}
 		return *this;
 	}
+
 	// returns the number of elements in the set.
 	int size() const {
-		return 0;
+		return _size;
 	}
+
 	// Returns the current size of the array.
 	int capacity() const {
-		return 0;
+		return _capacity;
 	}
+
 	// Removes a value to the set. Return true iff the element
 	// was successfully removed.
 	bool remove(int value) {
-		return false;
+		int loc = -1; // location of element to remove
+		for(int i = 0; i < _size; i++) {
+			if(data[i] == value) {
+				loc = i;
+				break;
+			}
+		}
+		if(loc > -1) {
+			// shift over elements in array
+			for(int i = loc; i < _size - 1; i++) {
+				data[i] = data[i+1];
+			}
+			_size--;
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	// Adds a value to the set. Return true iff the element
 	// was successfully added.
 	bool insert(int value) {
-		return false;
+		if(_size == 0) {
+			data[0] = value;
+			_size++;
+			return true;
+		}
+		else {
+			// Check to see if the set already contains the element
+			if(isElement(value)) {
+				return false;
+			}
+
+			// Resize the array if it's full
+			if(_size == _capacity) {
+				// resize the array
+				int new_capacity = _capacity + (_capacity / 2);
+				int* newData = new int[new_capacity];
+
+				memcpy(newData, data, _capacity * sizeof(int));
+
+				delete []data;
+				data = newData;
+				_capacity = new_capacity;
+			}
+
+			// Insert new element
+			data[_size] = value;
+			_size++;
+			return true;
+		}
 	}
+
 	// Returns true if value is part of set.
 	bool isElement(int value) const {
+		for(int i = 0; i < _size; i++) {
+			if(data[i] == value)
+				return true;
+		}
 		return false;
 	}
 	// Returns an array containing the elements in the set.
 	// This array is dynamically allocated an must be deleted by
 	// the caller.
 	int *getValues(int &sz) const {
-		sz = 5;
-		int *result = new int[sz];
-		result[0] = 5;
-		result[1] = 15;
-		result[2] = 20;
-		result[3] = 30;
-		result[4] = 40;
+		sz = _size;
+		int *result = new int[_size];
+		memcpy(result, data, _size * sizeof(int));
 		return result;
 	}
 };
